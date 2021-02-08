e971b2c804d8c91ff2f409209e4ea0747a1d5d29
timothymichaelcook <75735609+timothymichaelcook@users.noreply.github.com>
2021-02-08 07:32:26 -0500

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,24 @@
+#Comments in Python begin with # or """
+#Comments can be single or multi-line comments
+
+# are for single line comments
+# triple quotes 
+# """are for 
+# multi-line 
+# comments
+# """
+#print(test)
+"""
+print("test")
+"""
+print("test")
+
+#only will print last function
+
+"""
+print("test")
+"""
+
+"""
+print("test")
+"""
