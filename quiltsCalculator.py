cfa67255e7b5775580adc11a44b07c0e3d4314e5
timothymichaelcook <75735609+timothymichaelcook@users.noreply.github.com>
2021-02-08 07:53:32 -0500

    Add files via upload

---
+++ quiltsCalculator.py
@@ -0,0 +1,22 @@
+# Calculation of squares for different sized quilts:
+# 6x6 quilt
+quilt_width = 6
+quilt_length = 6
+print(quilt_width * quilt_length)
+
+# 7x7 quilt
+quilt_width = 7 
+quilt_length = 7
+print(quilt_width * quilt_length)
+
+# 8x8 quilt
+quilt_width = 8
+quilt_length = 8
+print(quilt_width * quilt_length)
+
+# How many squares for 6 people to have 6 quilts each that are 6x6?
+quilt_width = 6
+quilt_length = 6
+num_of_people = 6
+num_of_quilts = 6
+print(quilt_width * quilt_length * num_of_people * num_of_quilts)
