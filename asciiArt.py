d8b00771bfa7bff704fc17b1748915a8ca11ffe9
timothymichaelcook <75735609+timothymichaelcook@users.noreply.github.com>
2021-02-08 08:25:03 -0500

    Add files via upload

---
+++ asciiArt.py
@@ -0,0 +1,21 @@
+#THIS PROGRAM WILL DRAW YOUR INITIALS IN FORM OF ASCII ART (TYPE ART)
+
+initials_t = """
+TTTTT
+  T
+  T
+  T
+  T
+  T
+  T
+"""
+initials_c = """
+CCCCC
+C
+C
+C
+C
+C
+CCCCC
+"""
+print(initials_t, initials_c)
