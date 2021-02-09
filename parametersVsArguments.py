9d505a945540e9790b7f8b14468443ef305cf037
timothymichaelcook <75735609+timothymichaelcook@users.noreply.github.com>
2021-02-09 07:06:38 -0500

    Add files via upload

---
+++ parametersVsArguments.py
@@ -0,0 +1,27 @@
+#NO PARAMETER OR ARGUMENT
+
+def mult_two_add_three():
+    number = 5
+    print(number*2 + 3)
+  
+# Call mult_two_add_three() here:
+mult_two_add_three()
+
+#FUNCTION WITH PARAMETER AND ARGUMENT
+#
+#NUMBER IS THE FUNCTION'S PARAMETER
+#
+#6 IS THE FUNCTION'S ARGUMENT
+#
+def mult_two_add_three(number):
+      print(number*2 + 3)
+  
+# Call mult_two_add_three() here:
+mult_two_add_three(6)
+
+#FUNCTION WITH MULTIPLE PARAMETERS AND ARGUMENTS
+def mult_x_add_y(number, x, y):
+    print(number*x + y)
+
+mult_x_add_y(5, 2, 3)
+mult_x_add_y(1, 3, 1)
