9dd1bf5144832666304fb7f6a34a8c40ca43fdf1
timothymichaelcook <75735609+timothymichaelcook@users.noreply.github.com>
2021-02-09 06:54:10 -0500

    Add files via upload

---
+++ introToFunctions.py
@@ -0,0 +1,13 @@
+#THIS FUNCTIONS STORES SONG LYRICS AND PRINTS THEM WITH ONLY ONE LINE OF CODE BY CALLIGN THE sing_song FUNCTION
+#CALL A FUNCTION LIKE THIS: sing_song()
+
+def sing_song():
+    print("You may say I'm a dreamer")
+    print("But I'm not the only one")
+    print("I hope some day you'll join us")
+    print("And the world will be as one")
+  
+# call sing_song() below:
+sing_song()
+print("")
+sing_song()
